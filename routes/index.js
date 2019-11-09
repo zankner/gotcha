98b095f088602196cd94d0cb834204e5302de315
Ben Botvinick <benbotvinick@gmail.com>
2019-11-09 05:39:13 -0500

    route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const admin = require('firebase-admin');
-const moment = require('moment');
 const uniqid = require('uniqid');
 const status = require('http-status');
 
@@ -14,21 +13,28 @@ router.post('/tag', (req, res) => {
 	const token = req.body.token;
 	const lastWords = req.body.lastWords;
 
-	if(!token || !lastWords){
+	if (!token || !lastWords) {
 		return res.sendStatus(status.NOT_ACCEPTABLE);
 	}
 
 	admin.auth().verifyIdToken(token).then(decodedToken => {
 		const uid = decodedToken.email;
 		const userRef = admin.firestore().collection('users').doc(uid);
+
 		userRef.get().then(userDoc => {
 			const user = userDoc.data();
+			if (user.tagged) {
+				return res.sendStatus(status.UNAUTHORIZED);
+			}
+
 			userRef.collection('private').doc('userOnly').get().then(userOnlyDoc => {
 				const userOnly = userOnlyDoc.data();
+
 				userRef.collection('private').doc('adminOnly').get().then(adminOnlyDoc => {
 					const adminOnly = adminOnlyDoc.data();
 					const hunterUid = adminOnly.hunter;
 					const hunterRef = admin.firestore().collection('users').doc(hunterUid);
+
 					hunterRef.update({
 						'numTags': admin.firestore.FieldValue.increment(1)
 					}).then(() => {
@@ -41,34 +47,36 @@ router.post('/tag', (req, res) => {
 								const tagId = uniqid();
 								const tagRef = admin.firestore().collection('tags').doc(tagId);
 								const timestamp = Date.now();
+
 								tagRef.set({
 									timestamp: timestamp,
 									lastWords: lastWords,
 									name: user.name,
 									tagged: uid
-								}).then(()=> {
+								}).then(() => {
 									hunterRef.collection('private').doc('userOnly').update({
-										tags: admin.firestore.FieldValue.arrayUnion({[timestamp]: tagRef})
-									}).then(()=> {
+										tags: admin.firestore.FieldValue.arrayUnion({ [timestamp]: tagRef })
+									}).then(() => {
 										const statsRef = admin.firestore().collection('stats').doc('sumTags');
+
 										hunterRef.get().then((hunterDoc) => {
 											statsRef.update({
 												[hunterDoc.data().class]: admin.firestore.FieldValue.increment(1)
-											}).then(()=>{
+											}).then(() => {
 												admin.firestore().collection('users').doc(userOnly.target).collection('private').doc('adminOnly').update({
 													hunter: hunterUid
-												}).then(()=>{
+												}).then(() => {
 													res.sendStatus(status.OK);
 												});
-											})
-										})
-									})
-								})
-							})
-						})
-					})
-				})
-			})
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
 		});
 	});
 });
