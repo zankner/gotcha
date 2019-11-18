818e987075316b2aa67b1ca2cf79b391ea802c3b
Ben Botvinick <benbotvinick@gmail.com>
2019-11-17 20:33:26 -0500

    quick fix

--- components/StatsCard.js
+++ components/StatsCard.js
@@ -22,67 +22,69 @@ const StatsCard = props => {
 	}
 
 	return (
-		<div className="card">
-			<div className="card-header header text-uppercase">Statistics</div>
-			<div className="card-body p-4">
-				<div className="card">
-					<div className="card-header header text-uppercase">Tags by class</div>
-					<div className="p-3 card-body">
-					<Bar data={{
-						labels: [
-							'Freshman',
-							'Sophomores',
-							'Juniors',
-							'Seniors'
-						],
-						datasets: [{
-							label: 'Tags',
-							data: [sumTags['2023'], sumTags['2022'], sumTags['2021'], sumTags['2020']],
-							backgroundColor: [
-								'#ad4c43',
-								'#d1a49d',
-								'#bad2e3',
-								'#628bb9'
-							],
-							hoverBackgroundColor: [
-								'#ad4c43',
-								'#d1a49d',
-								'#bad2e3',
-								'#628bb9'
-							]
-						}]
-					}} options={{
-						responsive: true,
-						legend: {
-							display: false
-						},
-						scales: {
-							yAxes: [{
-								ticks: {
-									beginAtZero: true,
-									stepSize: 1
-								},
-								scaleLabel: {
-									display: true,
-									labelString: 'Tags',
-									fontSize: 17,
-									padding: 10
-								}
-							}],
-							xAxes: [{
-								gridLines: {
+		<div className={props.className}>
+			<div className="card">
+				<div className="card-header header text-uppercase">Statistics</div>
+				<div className="card-body p-4">
+					<div className="card">
+						<div className="card-header header text-uppercase">Tags by class</div>
+						<div className="p-3 card-body">
+							<Bar data={{
+								labels: [
+									'Freshman',
+									'Sophomores',
+									'Juniors',
+									'Seniors'
+								],
+								datasets: [{
+									label: 'Tags',
+									data: [sumTags['2023'], sumTags['2022'], sumTags['2021'], sumTags['2020']],
+									backgroundColor: [
+										'#ad4c43',
+										'#d1a49d',
+										'#bad2e3',
+										'#628bb9'
+									],
+									hoverBackgroundColor: [
+										'#ad4c43',
+										'#d1a49d',
+										'#bad2e3',
+										'#628bb9'
+									]
+								}]
+							}} options={{
+								responsive: true,
+								legend: {
 									display: false
+								},
+								scales: {
+									yAxes: [{
+										ticks: {
+											beginAtZero: true,
+											stepSize: 1
+										},
+										scaleLabel: {
+											display: true,
+											labelString: 'Tags',
+											fontSize: 17,
+											padding: 10
+										}
+									}],
+									xAxes: [{
+										gridLines: {
+											display: false
+										}
+									}]
 								}
-							}]
-						}
-					}} />
-				</div>
-					<ul className="list-group list-group-flush">
-						<li className="list-group-item"><strong>Freshmen:</strong> {sumTags['2023']}</li>
-						<li className="list-group-item"><strong>Sophomores:</strong> {sumTags['2022']}</li>
-						<li className="list-group-item"><strong>Juniors:</strong> {sumTags['2021']}</li>
-						<li className="list-group-item"><strong>Seniors:</strong> {sumTags['2020']}</li>
-					</ul>
+							}} />
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item"><strong>Freshmen:</strong> {sumTags['2023']}</li>
+							<li className="list-group-item"><strong>Sophomores:</strong> {sumTags['2022']}</li>
+							<li className="list-group-item"><strong>Juniors:</strong> {sumTags['2021']}</li>
+							<li className="list-group-item"><strong>Seniors:</strong> {sumTags['2020']}</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
