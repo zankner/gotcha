dcd67f2e1b5e7c3df536c513f346ac0926319116
Ben Botvinick <benbotvinick@gmail.com>
2019-11-19 08:30:25 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,8 @@ router.post('/tag', (req, res) => {
 		const uid = decodedToken.email;
 		const userRef = admin.firestore().collection('users').doc(uid);
 
+		console.log('Verified token');
+
 		userRef.get().then(userDoc => {
 			const user = userDoc.data();
 			if (user.tagged) {
@@ -52,12 +54,18 @@ router.post('/tag', (req, res) => {
 					hunterRef.update({
 						'numTags': admin.firestore.FieldValue.increment(1)
 					}).then(() => {
+						console.log('Updated hunter\'s tag count');
+
 						hunterRef.collection('private').doc('userOnly').update({
 							'target': userOnly.target
 						}).then(() => {
+							console.log('Updated hunter\'s target');
+
 							userRef.update({
 								'tagged': true
 							}).then(() => {
+								console.log('Updated player\'s tag status');
+
 								const tagId = uniqid();
 								const tagRef = admin.firestore().collection('tags').doc(tagId);
 								const timestamp = Date.now();
@@ -68,22 +76,32 @@ router.post('/tag', (req, res) => {
 									name: user.name,
 									tagged: uid
 								}).then(() => {
+									console.log('Created tag document');
+
 									hunterRef.collection('private').doc('userOnly').update({
 										tags: admin.firestore.FieldValue.arrayUnion({ [timestamp]: tagRef })
 									}).then(() => {
+										console.log('Assigned tag to hunter');
+
 										const statsRef = admin.firestore().collection('stats').doc('sumTags');
 
 										hunterRef.get().then((hunterDoc) => {
 											statsRef.update({
 												[hunterDoc.data().class]: admin.firestore.FieldValue.increment(1)
 											}).then(() => {
+												console.log('Updated statistics');
+
 												admin.firestore().collection('users').doc(userOnly.target).collection('private').doc('adminOnly').update({
 													hunter: hunterUid
 												}).then(() => {
-													console.log(typeof badgeCheck.firstTag);
+													console.log('Updated hunter\'s target');
+
 													badgeCheck.firstTag(user);
 													badgeCheck.tagStreak(hunterDoc.data());
 													badgeCheck.kingslayer(user);
+
+													console.log('Done');
+													
 													res.sendStatus(status.OK);
 												});
 											}).catch(() => {
